ac0f0bb8dcad81e1953e0aabc5815cb20247fb40
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-03-23 10:09:07 -0500

    created post form and request

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+//body parser is a middleware that tidies up the request object before we use them. use middleware with 'use'
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //get request
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html');
+})
+
+//post request
+app.post("/tasks", (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(3000, function() {
